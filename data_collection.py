91ef3f8524d35381f8bde1d436a040fffdccfd38
Darius-BA <kevindarius019@gmail.com>
2020-12-17 22:46:33 +0000

    uploading data and data collection script

--- data_collection.py
+++ data_collection.py
@@ -7,5 +7,5 @@ Created on Sun Nov 29 23:24:42 2020
 import glassdoor_scrapper as gs
 import pandas as pd
 
-path = "K:/Personal/Data Science salary prediction/chromedriver"
-df = gs.get_jobs("data analyst", 1000, False, path, 15)
+path = "K:/Personal/Data Analyst salary prediction/chromedriver"
+df = gs.get_jobs("data analyst", 950, False, path, 15)
